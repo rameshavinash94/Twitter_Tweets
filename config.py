5161a42cff7fda2439240f1b181a82333b405813
charucheema <89233727+charucheema@users.noreply.github.com>
2021-09-17 18:36:23 -0700

    Update config.py
    added comment

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+#--------------------------------------------------------------------
+# Twitter API and access keys
+# Author: charu.cheema@sjsu.edu
+#--------------------------------------------------------------------
+
 TWITTER_CONSUMER_KEY="6lVeu075jUCCm7qm8WHW3YW3i"
 TWITTER_CONSUMER_SECRET="o4Zj9dEG5HwaBsqqJOVtVs5a7gT6pVqTWiYE20jMMssG7Olqu6"
 TWITTER_ACCESS_TOKEN="1437856678842490883-ftZfZozIW3QuAtTBs0h4Ekb2qd3NkA"
